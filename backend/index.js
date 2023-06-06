613e90aa26fcd346a66bbd2042bbb855283f2ab8
balahari <balaharisankar2003@gmail.com>
2023-06-06 15:50:37 -0700

    Skillcert till 06-06-2023

--- backend/index.js
+++ backend/index.js
@@ -12,13 +12,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', 'https://linkbowl.netlify.app'||'http://localhost:3000');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-    next();
-  });
 app.get('/',(req,res)=>{
 	res.json({
 	"server":true
@@ -27,12 +20,20 @@ app.get('/',(req,res)=>{
 app.post('/create-pdf', (req, res) => {
     pdf.create(pdfTemplate(req.body), {}).toFile('result.pdf', (err) => {
         if(err) {
-            res.send(Promise.reject());
+            //res.send(Promise.reject()).json({
+	//status:false,
+	//message:"resume creation failed"
+	//});;
+	res.json({status:false,message:"Resume creation failed"})
         }
-        res.send(Promise.resolve());
+        //res.send(Promise.resolve()).json({
+	//status:true,
+	//message:"resume created"});
+        res.json({status:true,message:"Resume creation failed"})
+
     });
 });
-app.get('/fetch-pdf', (req, res) => {
+app.get('/resume', (req, res) => {
     res.sendFile(`${__dirname}/result.pdf`)
 })
 
