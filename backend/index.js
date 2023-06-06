50827d41a74fba5c1aa8b77d19e8bfb6cbe148c2
balahari <balaharisankar2003@gmail.com>
2023-06-06 13:35:34 -0700

    Skillcert till 06-06-2023

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,18 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', 'https://linkbowl.netlify.app'||'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+  });
+app.get('/',(req,res)=>{
+	res.json({
+	"server":true
+})
+})
 app.post('/create-pdf', (req, res) => {
     pdf.create(pdfTemplate(req.body), {}).toFile('result.pdf', (err) => {
         if(err) {
