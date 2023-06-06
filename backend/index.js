f3f504aa00cb795d160c9c339ca5a67432c6b46c
balahari <balaharisankar2003@gmail.com>
2023-06-06 16:42:38 -0700

    Skillcert till 06-06-2023

--- backend/index.js
+++ backend/index.js
@@ -10,20 +10,26 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.get('/',(req,res)=>{
-	res.json({
-	"server":true
-})
+app.get('/', (req, res) => {
+    res.json({
+        "server": true
+    })
 })
 app.post('/create-pdf', (req, res) => {
-    pdf.create(pdfTemplate(req.body),{}).toFile('./resume.pdf', (err,res) => {
-        if(err) {
-            return console.log(err)
-        }
-        console.log(res);
-    });
+    pdf.create(pdfTemplate(req.body), {
+        "height": "795px",        // allowed units: mm, cm, in, px
+        "width": "1122px",            // allowed units: mm, cm, in, px
+        "format": "A4",        // allowed units: A3, A4, A5, Legal, Letter, Tabloid
+        "orientation": "portrait",}).toFile('./resume.pdf', (err, res) => {
+            if (err) {
+                return console.log(err)
+            }
+            res.json({
+                message:res
+            })
+        });
 });
 app.get('/resume', (req, res) => {
     res.sendFile(`${__dirname}/resume.pdf`)
